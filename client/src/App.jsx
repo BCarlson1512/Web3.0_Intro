ddd85af54e0734774682761445dd067a87425957
Ben <46061491+BCarlson1512@users.noreply.github.com>
2022-04-27 23:37:38 -0400

    Components + Code Linting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
+import React from "react";
+import {Navbar, Welcome, Footer, Transactions, Services} from './components'
+
 const App = () => {
 
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   )
 }

832ebca69c00a99a6d61364ad4720246e26ae09d
Ben <46061491+BCarlson1512@users.noreply.github.com>
2022-04-28 17:39:27 -0400

    Connecting accounts via metamask

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import {AiFillPlayCircle} from 'react-icons/ai'
-import {SiEthereum} from 'react-icons/si'
-import {BsInfoCircle} from 'react-icons/bs'
-import {Loader} from './'
+import React, { useContext } from 'react'
+import { AiFillPlayCircle } from 'react-icons/ai'
+import { SiEthereum } from 'react-icons/si'
+import { BsInfoCircle } from 'react-icons/bs'
+
+import { TransactionContext } from '../context/TransactionContext'
+import { Loader } from './'
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
@@ -19,10 +21,7 @@ const Input = ({placeholder, name, type, value, handleChange}) => {
 }
 
 const Welcome = () => {
-  
-  const linkWallet = () => {
-
-  }
+  const { connectWallet, connectedAccount } = useContext(TransactionContext);
 
   const handleChange = () => {
   }
@@ -39,12 +38,14 @@ const Welcome = () => {
           <p className="text-white mt-5 text-left font-light md:w-9/12 w-11/12 text-base">
             Explore crypto... buy and sell crypto with ease on Krypt...
           </p>
-          <button 
-          type="button" 
-          onClick={linkWallet} 
-          className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
-            <p className="text-white text-base font-semibold">Connect Wallet</p>
-          </button>
+          {!connectedAccount && 
+            <button 
+            type="button" 
+            onClick={connectWallet} 
+            className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
+              <p className="text-white text-base font-semibold">Connect Wallet</p>
+            </button>
+          }
 
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${commonStyles}`}>

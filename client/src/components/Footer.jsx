e12d20876ec056d4aaf7b1501f7336048e4ba7c2
Ben <46061491+BCarlson1512@users.noreply.github.com>
2022-04-29 00:29:18 -0400

    Footer markup

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import logo from '../../images/logo.png';
 
 const Footer = () => {
   return (
-    <h1>Footer</h1>
+    <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
+      <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+        <div className="flex flex-[0.5] justify-center items-center">
+          <img src={logo} alt="logo" className="w-32"/>
+        </div>
+        <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
+          <p className="text-white text-base text-center mx-2 cursor-pointer">Market</p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">Exchange</p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">Tutorials</p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">Wallets</p>
+        </div>
+      </div>
+      <div className="mt-5 flex justify-center items-center flex-col">
+        <p className="text-white text-small text-center"> Join the revolution </p>
+      </div>
+    </div>
   )
 }
 

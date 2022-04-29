b4801581edd8267114fe3f1d2403427f875f38ef
Ben <46061491+BCarlson1512@users.noreply.github.com>
2022-04-28 23:00:45 -0400

    Update hardhat.config.js

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -7,8 +7,8 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/XyOaM61TEpQiTzSKAMYUbExK3PUBqN4p',
-      accounts: [process.env.ETH_ROPSTEN_PRIV_KEY]
+      url: process.env.ETH_API_KEY,
+      accounts: [`0x${process.env.ETH_ROPSTEN_PRIV_KEY}`]
     }
   }
 }
